6082fcddbce3725663da5b7fefb0f529a765fe25
Lukas Welte <ich@lukaswelte.de>
2015-02-14 18:51:23 +0100

    Added jump distance to data

--- JumpAnalysis/CaptureJumpViewController.swift
+++ JumpAnalysis/CaptureJumpViewController.swift
@@ -68,7 +68,7 @@ class CaptureJumpViewController: UIViewController {
     func measurementDidFinish() {
         let upperSensorData = self.sensorDataSession.allSensorData()
         let sensorDataDictionaries = upperSensorData.map({sensorData in sensorData.toDictionary()})
-        let jumpDictionary = ["id":self.jumpNumber(),"name":self.nameTextField.text, "weightInKg":convertStringToDouble(self.weightInKgTextField.text), "heightInMeter":convertStringToDouble(self.heightInMeterTextField.text), "additionalInformation":self.additionalInformationTextField.text, "leftFoot": self.leftFootSwitch.on, "sensorData":sensorDataDictionaries]
+        let jumpDictionary = ["id":self.jumpNumber(),"name":self.nameTextField.text, "weightInKg":convertStringToDouble(self.weightInKgTextField.text), "heightInMeter":convertStringToDouble(self.heightInMeterTextField.text), "additionalInformation":self.additionalInformationTextField.text, "leftFoot": self.leftFootSwitch.on, "jumpDistanceInCm":0, "jumpDurationInMs":0, "sensorData":sensorDataDictionaries]
         let json = JSON(jumpDictionary)
         let jsonString = json.description
         FileHandler.writeToFile("\(self.jumpNumber()).json", content: jsonString)
